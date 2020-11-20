0159bff28eb89ae45ce19b11ca074fdcddb6c7b3
TiagooGomess <tiagogomes2011@live.com.pt>
2020-11-20 16:30:56 +0000

    Added comments to the predicates

--- src/bots.pl
+++ src/bots.pl
@@ -4,26 +4,29 @@
 
 :- use_module(library(between)).
 
+% este predicado dá-nos um movimento aleatório válido
 getMoveEasy(GameState, Player, RowStart, ColumnStart, RowEnd, ColumnEnd):-
     getPiecePosFrom(GameState, Player, RowStart, ColumnStart).
 	getPiecePosTo(GameState, RowStart, ColumnStart, RowEnd, ColumnEnd).
 
+% este predicado dá-nos uma linha e uma coluna aleatórias, entre 0 e 5
 getRandomRowAndColumn(Row,Column):-
     between(0,5,Row),
     between(0,5,Column).
 
+% escolhe uma peça válida para mover de forma aleatória
 getPiecePosFrom(Board, Player, Row, Column):-
 	repeat,
 	getRandomRowAndColumn(Row,Column),
 	(
 		(
-			getPieceByRowAndColumn(Board, Row, Column, Piece),
-   	 		Piece = Player,
-			\+ checkIfStackCannotCapture(Board, Row, Column),!
+   	 		Piece = Player, % verifica se a stack pertence ao jogador atual
+			\+ checkIfStackCannotCapture(Board, Row, Column),! % verifica se a stack pode capturar outras stacks
 		);
 		fail
 	).
-	
+
+% escolhe uma posição final válida de forma aleatória
 getPiecePosTo(Board, RowFrom, ColumnFrom, RowTo, ColumnTo):-
 	repeat,
 	getRandomRowAndColumn(Row,Column),
@@ -35,4 +38,3 @@ getPiecePosTo(Board, RowFrom, ColumnFrom, RowTo, ColumnTo):-
 		);
 		fail
 	).
-	
